eff4d8544b6f665bc7e7e46841878e0b6326734f
purpletianjing <TianjingSun@outlook.com>
2016-04-07 10:52:04 +0800

    add groupUser for GroupUsers

--- src/main/java/com/thoughtworks/twars/bean/GroupUsers.java
+++ src/main/java/com/thoughtworks/twars/bean/GroupUsers.java
@@ -1,14 +1,8 @@
 package com.thoughtworks.twars.bean;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 public class GroupUsers {
     private int groupId;
     private int userId;
-//    private List<Group> groups;
 
     public int getUserId() {
         return userId;
@@ -26,16 +20,4 @@ public class GroupUsers {
         this.groupId = groupId;
     }
 
-//    public List<Map> getResponseInfo(){
-//
-//        List<Map> groupsInfo = groups.stream()
-//                .map(group -> {
-//                    Map groupMap = new HashMap();
-//                    groupMap.put("id", group.getId());
-//                    groupMap.put("name", group.getName());
-//                    groupMap.put("avatar", group.getAvatar());
-//                    return groupMap;
-//                }).collect(Collectors.toList());
-//        return groupsInfo;
-//    }
 }
