3e9e690fe2737209a40208731efb4909989b569c
wengjiaojiao <419909058@qq.com>
2016-04-12 11:32:17 +0800

    modify group structure

---
+++ src/main/resources/db/migration/V6__Modify_groups_structure.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `group` ADD COLUMN `memberNumber` int;
---
+++ src/test/resources/db/migration/V6_1__Add_test_data_for_group_table.sql
@@ -0,0 +1,3 @@
+UPDATE `group` SET `announcement` = "公告公告公告", `avatar` = "头像一", `isAnnouncePublished` = true where `id` = 1;
+UPDATE `group` SET `announcement` = "公告公告公告", `avatar` = "头像二", `isAnnouncePublished` = true where `id` = 2;
+UPDATE `group` SET `announcement` = "公告公告公告", `avatar` = "头像三", `isAnnouncePublished` = true where `id` = 3
