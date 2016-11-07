4a21023caf38e4d203d733f3834377d5abcb21b0
Yvette14 <libratyb1014@163.com>
2016-11-07 17:11:35 +0800

    modify login service

--- src/main/java/com/thoughtworks/twars/App.java
+++ src/main/java/com/thoughtworks/twars/App.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.twars;
 
+import com.thoughtworks.twars.bean.LoginDetail;
+import com.thoughtworks.twars.mapper.LoginDetailMapper;
 import com.thoughtworks.twars.mapper.UserMapper;
 import com.thoughtworks.twars.util.DBUtil;
 import org.apache.ibatis.session.SqlSessionManager;
@@ -13,11 +15,13 @@ public class App extends ResourceConfig {
     public App(){
         SqlSessionManager session = (SqlSessionManager) DBUtil.getSession();
         final UserMapper userMapper = session.getMapper(UserMapper.class);
+        final LoginDetailMapper loginDetailMapper = session.getMapper(LoginDetailMapper.class);
         packages("com.thoughtworks.twars.resource")
                 .register(new AbstractBinder() {
                     @Override
                     protected void configure() {
                         bind(userMapper).to(UserMapper.class);
+                        bind(loginDetailMapper).to(LoginDetailMapper.class);
                     }
                     //将上面创建的userMapper赋给所有用UserMapper类创建的对象，就是resource文件夹中private UserMapper userMapper；自动使用了此处的usermapper
                 });
